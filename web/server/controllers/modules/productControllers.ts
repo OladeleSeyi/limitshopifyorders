5be7a122e70f233a09609a92b246b5439e1c644f
oladeleseyi <toothy61@gmail.com>
2022-09-12 11:00:18 +0100

    register webhook

--- web/server/controllers/modules/productControllers.ts
+++ web/server/controllers/modules/productControllers.ts
@@ -1,4 +1,5 @@
 import { Context } from "koa";
+import { Webhook } from "../../app";
 import { ProductDb } from "../../database";
 
 export const productController = async (ctx: Context) => {
@@ -7,11 +8,17 @@ export const productController = async (ctx: Context) => {
   // verify validy of product
   // register webhook listener
   const { body } = ctx.request;
+  const { shop, accessToken } = ctx.state.shopify;
   try {
     await ProductDb.findOneAndUpdate({ shopifyId: body.productId }, body, {
       upsert: true,
       setDefaultsOnInsert: true,
     });
+    const webhook = new Webhook(shop, accessToken);
+
+    await webhook.register(process.env.HOST!, "carts/create", "CARTS_CREATE");
+
+    await webhook.register(process.env.HOST!, "carts/update", "CARTS_UPDATE");
   } catch (error) {
     ctx.body = {
       statusCode: 400,
